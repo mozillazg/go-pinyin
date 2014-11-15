c93c3116c5a71185064a6d83431235e2d5e50681
mozillazg <opensource.mozillazg@gmail.com>
2014-11-15 15:38:29 +0800

    add examples

---
+++ examples/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"github.com/mozillazg/go-pinyin"
+)
+
+func main() {
+	hans := "中国人"
+	fmt.Println("default: ",
+		pinyin.Pinyin(hans, pinyin.Args{}),
+	)
+	fmt.Println("NORMAL: ",
+		pinyin.Pinyin(hans, pinyin.Args{Style: pinyin.NORMAL}),
+	)
+	fmt.Println("TONE: ",
+		pinyin.Pinyin(hans, pinyin.Args{Style: pinyin.TONE}),
+	)
+	fmt.Println("TONE2: ",
+		pinyin.Pinyin(hans, pinyin.Args{Style: pinyin.TONE2}),
+	)
+	fmt.Println("INITIALS: ",
+		pinyin.Pinyin(hans, pinyin.Args{Style: pinyin.INITIALS}),
+	)
+	fmt.Println("FIRST_LETTER: ",
+		pinyin.Pinyin(hans, pinyin.Args{Style: pinyin.FIRST_LETTER}),
+	)
+	fmt.Println("FINALS: ",
+		pinyin.Pinyin(hans, pinyin.Args{Style: pinyin.FINALS}),
+	)
+	fmt.Println("FINALS_TONE: ",
+		pinyin.Pinyin(hans, pinyin.Args{Style: pinyin.FINALS_TONE}),
+	)
+	fmt.Println("FINALS_TONE2: ",
+		pinyin.Pinyin(hans, pinyin.Args{Style: pinyin.FINALS_TONE2}),
+	)
+	fmt.Println("Heteronym true: ",
+		pinyin.Pinyin(hans, pinyin.Args{Heteronym: true}),
+	)
+	fmt.Println("Heteronym true INITIALS: ",
+		pinyin.Pinyin(hans, pinyin.Args{Heteronym: true,
+			Style: pinyin.INITIALS},
+		),
+	)
+}
