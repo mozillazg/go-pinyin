fdf044821f84fef24731cef03ee2a331cda14924
mozillazg <opensource.mozillazg@gmail.com>
2016-03-12 10:05:37 +0800

    命令行程序支持从标准输入读取数据

--- pinyin/main.go
+++ pinyin/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"strings"
+
+	"github.com/mozillazg/go-pinyin"
+	"golang.org/x/crypto/ssh/terminal"
 )
-import "github.com/mozillazg/go-pinyin"
 
 func main() {
 	heteronym := flag.Bool("e", false, "启用多音字模式")
@@ -14,9 +17,16 @@ func main() {
 	hans := flag.Args()
 	args := pinyin.NewArgs()
 	args.Style = pinyin.Tone
+	stdin := []byte{}
+	if !terminal.IsTerminal(0) {
+		stdin, _ = ioutil.ReadAll(os.Stdin)
+	}
+	if len(stdin) > 0 {
+		hans = append(hans, string(stdin))
+	}
 
 	if len(hans) == 0 {
-		fmt.Println("请至少输入一个汉字: pinyin hans [hans ...]")
+		fmt.Println("请至少输入一个汉字: pinyin HANS [HANS ...]")
 		os.Exit(1)
 	}
 	if *heteronym {
